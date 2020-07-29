5134496c8031f9d139e65d3951c3d96e98af3230
SusmithHCK <tesla8.1997@gmail.com>
2020-07-29 15:27:37 +0530

    Fixed undefined on searchbar

---
+++ src/components/nativePages/Osint/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+class Osint extends React.Component {
+  constructor(props){
+    super(props)
+
+  }
+  render(){
+    return(
+      <>
+        <h1>OSINT arsenal</h1>
+      </>
+    )
+  }
+}
+export default Osint
