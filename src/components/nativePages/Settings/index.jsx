5a51b7d5d9ae5e2317524dd6d7ef02abb0b2713c
Basil K Y <techiebasil@gmail.com>
2020-09-04 22:11:39 +0530

    updated settings page

--- src/components/nativePages/Settings/index.jsx
+++ src/components/nativePages/Settings/index.jsx
@@ -3,11 +3,14 @@ import './main.css'
 import bingImg from '../../../assets/images/icons8-bing-96.png'
 import googleImg from '../../../assets/images/icons8-google-96.png'
 import duckImg from '../../../assets/images/icons8-duckduckgo-96.png'
-import ecosiaImg from '../../../assets/images/icons8-ecosia-96.png'
+import ecosiaImg from '../../../assets/images/ecosia.png'
 import yahooImg from '../../../assets/images/yahoo-icon.png'
 import yandexImg from '../../../assets/images/yandex-icon.png'
 import qwantImg from '../../../assets/images/qwant-icon.png'
+import { act } from 'react-dom/test-utils'
 
+const { dialog } = window.require('electron').remote
+const { ipcRenderer } = window.require('electron')
 const remote = window.require('electron').remote
 const app = remote.app
 const configFilePath = app.getPath('userData') + '/preferences.json'
@@ -19,8 +22,10 @@ class BlankTab extends React.Component {
     console.log(props)
     this.state = {
       pref: {
-        searchEngine: 'google'
-      }
+        searchEngine: 'google',
+        downloadLocation: app.getPath('downloads')
+      },
+      active: 'settings'
     }
   }
   privateToggleChange = e => {
@@ -53,93 +58,191 @@ class BlankTab extends React.Component {
     })
     this.props.handleSearchEngineChange(this.state.pref.searchEngine)
   }
+  selectDownloadLocation = async () => {
+    let pref = this.state.pref
+    let path = await dialog.showOpenDialog({
+      properties: ['openDirectory']
+    })
+
+    pref.downloadLocation = path.filePaths[0]
+    ipcRenderer.send('change_download_setting', pref)
+    this.setState({ pref }, this.savePreference)
+  }
 
   render () {
     return (
-      <div className='settings-container'>
-        <div className='border-left container p-5 '>
-          <h4>Search engine</h4>
-          <br />
-          <div className='settings-search-engine-list-ctr'>
-            <div className='p-1 mr-3'>
-              <span>Recomended</span>
-              <div className='settings-search-engine-list pt-3'>
-                <img
-                  className={
-                    this.state.pref.searchEngine === 'ddg' ? 'active' : null
-                  }
-                  onClick={() => this.searchEngineSelector('ddg')}
-                  title='DuckDuckGo'
-                  src={duckImg}
-                  alt='DuckDuckGo'
-                />
-                <img
-                  className={
-                    this.state.pref.searchEngine === 'ecosia' ? 'active' : null
-                  }
-                  onClick={() => this.searchEngineSelector('ecosia')}
-                  title='Ecosia'
-                  src={ecosiaImg}
-                  alt='Ecosia'
-                />
-                <img
-                  className={
-                    this.state.pref.searchEngine === 'google' ? 'active' : null
-                  }
-                  onClick={() => this.searchEngineSelector('google')}
-                  title='google'
-                  src={googleImg}
-                  alt='Google'
-                />
-              </div>
-            </div>
-            <div className='ml-3 p-1'>
-              <span>Popular</span>
-              <div className='settings-search-engine-list pt-3'>
-                <img
-                  className={
-                    this.state.pref.searchEngine === 'qwant' ? 'active' : null
-                  }
-                  onClick={() => this.searchEngineSelector('qwant')}
-                  title='Qwant'
-                  src={qwantImg}
-                  alt='Qwant'
-                />
-                <img
-                  className={
-                    this.state.pref.searchEngine === 'yandex' ? 'active' : null
-                  }
-                  onClick={() => this.searchEngineSelector('yandex')}
-                  title='Yandex'
-                  src={yandexImg}
-                  alt='Yandex'
-                />
-                <img
-                  className={
-                    this.state.pref.searchEngine === 'bing' ? 'active' : null
-                  }
-                  onClick={() => this.searchEngineSelector('bing')}
-                  title='Bing'
-                  src={bingImg}
-                  alt='Bing'
-                />
-                <img
-                  className={
-                    this.state.pref.searchEngine === 'yahoo' ? 'active' : null
-                  }
-                  onClick={() => this.searchEngineSelector('yahoo')}
-                  title='Yahoo'
-                  src={yahooImg}
-                  alt='Yahoo'
-                />
-              </div>
+      <div className='settings-container h-100 pt-2'>
+        <div className='row'>
+          <div className='col-sm-3 pt-3'>
+            <div className='pl-4'>
+              <p
+                className={
+                  this.state.active === 'settings' ? 'h3 mt-1' : 'mt-1'
+                }
+                onClick={() => {
+                  this.setState({ active: 'settings' })
+                }}
+                role='button'
+              >
+                Settings
+              </p>
+              <p
+                className={
+                  this.state.active === 'downloads' ? 'h3 mt-3' : 'mt-3'
+                }
+                onClick={() => {
+                  this.setState({ active: 'downloads' })
+                }}
+                role='button'
+              >
+                Downloads
+              </p>
+              <p
+                className={this.state.active === 'about' ? 'h3 mt-3' : 'mt-3'}
+                onClick={() => {
+                  this.setState({ active: 'about' })
+                }}
+                role='button'
+              >
+                About
+              </p>
             </div>
           </div>
-
-          <div className='mt-5 col-lg-8' hidden>
-            <button className='btn btn-primary' onClick={this.savePreference}>
-              Save
+          <div className='col-sm-9 pt-3'>
+            <h4>Search engine</h4>
+            <br />
+            <div className='settings-search-engine-list-ctr'>
+              <div className='p-1 mr-3'>
+                <span>Recomended</span>
+                <div className='settings-search-engine-list pt-3'>
+                  <img
+                    className={
+                      this.state.pref.searchEngine === 'ddg' ? 'active' : null
+                    }
+                    onClick={() => this.searchEngineSelector('ddg')}
+                    title='DuckDuckGo'
+                    src={duckImg}
+                    alt='DuckDuckGo'
+                  />
+                  <img
+                    className={
+                      this.state.pref.searchEngine === 'ecosia'
+                        ? 'active'
+                        : null
+                    }
+                    onClick={() => this.searchEngineSelector('ecosia')}
+                    title='Ecosia'
+                    src={ecosiaImg}
+                    alt='Ecosia'
+                  />
+                  <img
+                    className={
+                      this.state.pref.searchEngine === 'google'
+                        ? 'active'
+                        : null
+                    }
+                    onClick={() => this.searchEngineSelector('google')}
+                    title='google'
+                    src={googleImg}
+                    alt='Google'
+                  />
+                </div>
+              </div>
+              <div className='ml-3 p-1'>
+                <span>Popular</span>
+                <div className='settings-search-engine-list pt-3'>
+                  <img
+                    className={
+                      this.state.pref.searchEngine === 'qwant' ? 'active' : null
+                    }
+                    onClick={() => this.searchEngineSelector('qwant')}
+                    title='Qwant'
+                    src={qwantImg}
+                    alt='Qwant'
+                  />
+                  <img
+                    className={
+                      this.state.pref.searchEngine === 'yandex'
+                        ? 'active'
+                        : null
+                    }
+                    onClick={() => this.searchEngineSelector('yandex')}
+                    title='Yandex'
+                    src={yandexImg}
+                    alt='Yandex'
+                  />
+                  <img
+                    className={
+                      this.state.pref.searchEngine === 'bing' ? 'active' : null
+                    }
+                    onClick={() => this.searchEngineSelector('bing')}
+                    title='Bing'
+                    src={bingImg}
+                    alt='Bing'
+                  />
+                  <img
+                    className={
+                      this.state.pref.searchEngine === 'yahoo' ? 'active' : null
+                    }
+                    onClick={() => this.searchEngineSelector('yahoo')}
+                    title='Yahoo'
+                    src={yahooImg}
+                    alt='Yahoo'
+                  />
+                </div>
+              </div>
+            </div>
+            <br />
+            <h4>Download Location</h4>
+            <br />
+            <button
+              className={
+                this.state.pref.downloadLocation !== 'ask'
+                  ? 'download download-active'
+                  : 'download'
+              }
+              onClick={() => {
+                var pref = { ...this.state.pref }
+                pref.downloadLocation = app.getPath('downloads')
+                ipcRenderer.send('change_download_setting', pref)
+                this.setState({ pref }, this.savePreference)
+              }}
+            >
+              Custom
             </button>
+            <button
+              className={
+                this.state.pref.downloadLocation === 'ask'
+                  ? 'ml-4 download download-active'
+                  : 'ml-4 download'
+              }
+              onClick={() => {
+                var pref = { ...this.state.pref }
+                pref.downloadLocation = 'ask'
+                ipcRenderer.send('change_download_setting', pref)
+                this.setState({ pref }, this.savePreference)
+              }}
+            >
+              Ask Everytime
+            </button>
+            <br />
+            <button
+              className={
+                this.state.pref.downloadLocation !== 'ask'
+                  ? 'mt-3 download'
+                  : 'd-none'
+              }
+              onClick={() => this.selectDownloadLocation()}
+              style={{ width: '500px', textAlign: 'left' }}
+            >
+              {this.state.pref.downloadLocation}
+            </button>
+            <br />
+            <br />
+            <p className='small'>
+              <i className='fa fa-info-circle'></i> Download preference changes
+              will be reflected on next start.
+            </p>
           </div>
         </div>
       </div>
