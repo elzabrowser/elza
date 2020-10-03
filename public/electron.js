71cda169b7d296192e9c312ca54b3f9138e34db3
Basil K Y <techiebasil@gmail.com>
2020-10-03 22:43:30 +0530

    disable new tor window for now

--- public/electron.js
+++ public/electron.js
@@ -41,7 +41,7 @@ updateDownload = () => {
 
 tor.connect_tor()
 if (require(configFilePath).isTorEnabled) {
-  //app.commandLine.appendSwitch('proxy-server', 'socks5://127.0.0.1:9050')
+  app.commandLine.appendSwitch('proxy-server', 'socks5://127.0.0.1:9050')
 }
 
 app.on('ready', function () {
@@ -173,6 +173,43 @@ app.on('web-contents-created', (e, contents) => {
       showSaveLinkAs: true,
       showSearchWithGoogle: false,
       window: contents
+    })
+  }
+})
+electronDl({
+  saveAs: askLocation,
+  showBadge: true,
+  directory: downloadLocation,
+  onStarted: function (item) {
+    var downloadItem = new Object()
+    downloadItem.name = item.getFilename()
+    downloadItem.totalBytes = item.getTotalBytes()
+    downloadItem.receivedBytes = 0
+    var d = new Date()
+    var downloadID = d.getTime()
+    downloadItem.status = 'started'
+    downloadItem.path = item.getSavePath()
+    downloads[downloadID] = downloadItem
+    updateDownload()
+    item.once('done', (event, state) => {
+      if (state === 'completed') {
+        downloads[downloadID].status = 'done'
+        updateDownload()
+      } else {
+        console.log(`Download failed: ${state}`)
+      }
+    })
+    item.on('updated', (event, state) => {
+      if (state === 'interrupted') {
+        console.log('Download is interrupted but can be resumed')
+      } else if (state === 'progressing') {
+        if (item.isPaused()) {
+          console.log('Download is paused')
+        } else {
+          downloads[downloadID].receivedBytes = item.getReceivedBytes()
+          updateDownload()
+        }
+      }
     })
   }
 })
