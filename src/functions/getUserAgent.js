24c4e7f9c412f6a90ee42c26b0c3656cdd7f28c4
Basil K Y <techiebasil@gmail.com>
2020-10-18 16:44:29 +0530

    update useragent

--- src/functions/getUserAgent.js
+++ src/functions/getUserAgent.js
@@ -1,2 +1,3 @@
-var USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/84.0.4147.89 Safari/537.36'
+var USER_AGENT =
+  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280 Safari/537.36'
 export default USER_AGENT
