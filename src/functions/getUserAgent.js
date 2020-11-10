bbe9894b77cd19b48d41e5b7636958a062694a34
Basil K Y <techiebasil@gmail.com>
2020-11-10 22:46:27 +0530

    change useragent to firefox

--- src/functions/getUserAgent.js
+++ src/functions/getUserAgent.js
@@ -1,3 +1,3 @@
 var USER_AGENT =
-  'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280 Safari/537.36'
+  'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:77.0) Gecko/20100101 Firefox/83.0'
 export default USER_AGENT
