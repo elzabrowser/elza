d442b45364cd9e56699cdcd4a8527dac289effb2
Basil K Y <techiebasil@gmail.com>
2021-05-30 09:16:48 +0530

    update notifier

--- public/functions/notifier.js
+++ public/functions/notifier.js
@@ -1,8 +1,9 @@
 const { app } = require('electron')
 const { Notification } = require('electron')
-const fs = require('fs')
 const semver = require('semver')
 const preference = require('./config')
+const getJSON = require('get-json')
+const notificationURL = 'https://elzabrowser.com/notifications.json'
 function showNotification (title, message, callback) {
   var notification = new Notification({
     title: title,
@@ -15,32 +16,39 @@ function showNotification (title, message, callback) {
 }
 
 function checkMessages () {
-  let version = app.getVersion()
-  console.log(version)
-  let notifications = JSON.parse(fs.readFileSync('notification.json', 'utf8'))
-  visitedNotifications = preference.getPreference().notifications
-  for (let notification in notifications) {
-    if (
-      !visitedNotifications.includes(notification) &&
-      (notifications[notification].versions.includes('all') ||
-        notifications[notification].versions.includes(version) ||
-        (notifications[notification].olderthan &&
-          semver.lte(version, notifications[notification].olderthan)))
-    ) {
-      console.log(notification)
-      showNotification(
-        notifications[notification].title,
-        notifications[notification].message,
-        () => {
-          preference.setPreference(
-            'notifications',
-            visitedNotifications.concat(notification)
+  try {
+    let version = app.getVersion()
+    getJSON(notificationURL, function (error, notifications) {
+      if (error) return
+      console.log(notifications)
+      visitedNotifications = preference.getPreference().notifications
+      for (let notification in notifications) {
+        if (
+          !visitedNotifications.includes(notification) &&
+          (notifications[notification].versions.includes('all') ||
+            notifications[notification].versions.includes(version) ||
+            (notifications[notification].olderthan &&
+              semver.lte(version, notifications[notification].olderthan)))
+        ) {
+          console.log(notification)
+          showNotification(
+            notifications[notification].title,
+            notifications[notification].message,
+            () => {
+              preference.setPreference(
+                'notifications',
+                visitedNotifications.concat(notification)
+              )
+            }
           )
         }
-      )
-    }
+      }
+    })
+  } catch (e) {
+    console.log(e)
   }
 }
+
 app.on('ready', function () {
   setTimeout(() => {
     checkMessages()
