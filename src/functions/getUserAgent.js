1fc8278b7d736f9a9d2e7d96ddf1b9889eb859d2
Basil K Y <techiebasil@gmail.com>
2021-06-21 23:18:49 +0530

    get platform from main process as process.platform is undefined in renderer

--- src/functions/getUserAgent.js
+++ src/functions/getUserAgent.js
@@ -3,10 +3,11 @@
 var USER_AGENT =
   'Mozilla/5.0 (X11; Linux x86_64; rv:VERSION) Gecko/20100101 Firefox/VERSION'
 var currentVersion = '90.0'
-if (process.platform === 'win32')
+var platform = window.preloadAPI.send('getPlatform', '', true)
+if (platform === 'win32')
   USER_AGENT =
     'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:VERSION) Gecko/20100101 Firefox/VERSION'
-else if (process.platform === 'darwin')
+else if (platform === 'darwin')
   USER_AGENT =
     'Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.10; rv:VERSION) Gecko/20100101 Firefox/VERSION'
 export default USER_AGENT.replaceAll('VERSION', currentVersion)
